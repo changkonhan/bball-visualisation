53daa5dfc1744f3406e19cc232d18116dc5df841
Chang Kon Han <chang_kon@hotmail.com>
2019-04-12 01:20:07 +0900

    Run both server and client simultaneously

--- src/server/index.js
+++ src/server/index.js
@@ -5,13 +5,13 @@ const port = 3001;
 const stats = require("./routes/StatsApi");
 
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 app.use("/stats", stats);
 
 app.listen(port, () => {
-	console.log(`Server running on port ${port}`)
+    console.log(`Server running on port ${port}`);
 });
--- src/server/routes/StatsApi.js
+++ src/server/routes/StatsApi.js
@@ -4,16 +4,16 @@ var nbaService = require("../services/NBAService");
 var router = express.Router();
 
 router.get("/shotchartdetail", function(req, res) {
-	let team = req.query.team;
-	let season = req.query.season;
+    let team = req.query.team;
+    let season = req.query.season;
 
-	nbaService.getTeamShotChart(team, season).then(function(value) {
-		let payload = value.data;
-		res.send(payload);
-	}).catch(function(err) {
-		console.log(err);
-		res.send("Error occurred!");
-	});
+    nbaService.getTeamShotChart(team, season).then(function(value) {
+        let payload = value.data;
+        res.send(payload);
+    }).catch(function(err) {
+        console.log(err);
+        res.send("Error occurred!");
+    });
 });
 
 module.exports = router;
